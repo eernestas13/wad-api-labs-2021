ab5285aa8fd7ea5e40693bcedaada93fbfff30d3
Ernestas Trakys <20092082@mail.wit.ie>
2021-12-06 19:47:11 +0000

    mongo lab user favourites

--- movies-api/api/users/index.js
+++ movies-api/api/users/index.js
@@ -9,6 +9,16 @@ router.get('/', async (req, res) => {
     res.status(200).json(users);
 });
 
+// Get Favourites
+router.get('/:id/favourites', async (req, res) => {
+    const user = await User.findById(req.params.id);
+    if (user) {
+        res.status(200).json(user.favourites);
+    } else {
+        res.status(404).json({ code: 404, msg: 'Unable to find favourites' });
+    }
+});
+
 // register(Create)/Authenticate User
 router.post('/', async (req, res) => {
     if (req.query.action === 'register') {  //if action is 'register' then save to DB
@@ -38,6 +48,21 @@ router.put('/:id', async (req, res) => {
         res.status(200).json({ code:200, msg: 'User Updated Sucessfully' });
     } else {
         res.status(404).json({ code: 404, msg: 'Unable to Update User' });
+    }
+});
+
+// Add favourites
+router.post('/:id/favourites', async (req, res) => {
+    const newFavourite = req.body;
+    if (newFavourite && newFavourite.id) {
+        const user = await User.findById(req.params.id);
+        if (user) {
+            user.favourites.push(newFavourite);
+            user.save();
+            res.status(201).json({ code: 201, msg: "Added Favourite" });
+        } else {
+            res.status(404).json({ code: 404, msg: 'Unable to add favourites' });
+        }
     }
 });
 
