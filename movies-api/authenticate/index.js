7f8f4a24a32c37b778358a65c8ea0709e1436cf0
Ernestas Trakys <20092082@mail.wit.ie>
2021-12-07 18:21:18 +0000

    topic 10 - passport

--- movies-api/authenticate/index.js
+++ movies-api/authenticate/index.js
@@ -1,4 +1,4 @@
-import User from '../api/users/userModel';
+/*import User from '../api/users/userModel';
 // Authentication and Authorization Middleware
 export default async (req, res, next) => {
   if (req.session && req.session.authenticated) {
@@ -9,4 +9,29 @@ export default async (req, res, next) => {
   } else {
     return res.status(401).json({status:401,message:"unauthorised"});
   }
-};
+}; */
+
+import passport from 'passport';
+import passportJWT from 'passport-jwt';
+import UserModel from './../api/users/userModel';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const JWTStrategy = passportJWT.Strategy;
+const ExtractJWT = passportJWT.ExtractJwt;
+
+let jwtOptions = {};
+jwtOptions.jwtFromRequest = ExtractJWT.fromAuthHeaderAsBearerToken();
+jwtOptions.secretOrKey = process.env.SECRET;
+const strategy = new JWTStrategy(jwtOptions, async (payload, next) => {
+  const user = await UserModel.findByUserName(payload);
+  if (user) {
+    next(null, user);
+  } else {
+    next(null, false);
+  }
+});
+passport.use(strategy);
+
+export default passport;
--- movies-api/index.js
+++ movies-api/index.js
@@ -6,7 +6,7 @@ import moviesRouter from './api/movies';
 import genresRouter from './api/genres';
 import usersRouter from './api/users';
 import session from 'express-session';
-import authenticate from './authenticate';
+import passport from './authenticate';
 
 dotenv.config();
 
@@ -25,14 +25,9 @@ const port = process.env.PORT;
 
 app.use(express.json());
 
-//session middleware
-app.use(session({
-  secret: 'ilikecake',
-  resave: true,
-  saveUninitialized: true
-}));
+app.use(passport.initialize());
 
-app.use('/api/movies', authenticate, moviesRouter);
+app.use('/api/movies', passport.authenticate('jwt', {session: false}), moviesRouter);
 
 app.use('/api/genres', genresRouter);
 
