dfc9a1a7b057a079db31066b538eaef499b2ce3d
Ernestas Trakys <20092082@mail.wit.ie>
2021-12-07 18:44:53 +0000

    topic 10 - update movies data/route

--- movies-api/seedData/index.js
+++ movies-api/seedData/index.js
@@ -1,6 +1,10 @@
 import userModel from '../api/users/userModel';
 import users from './users';
 import dotenv from 'dotenv';
+import movieModel from '../api/movies/movieModel';
+import movies from './movies.js';
+import genres from './genres.js';
+import genresModel from '../api/genres/genresModel';
 
 dotenv.config();
 
@@ -16,6 +20,33 @@ async function loadUsers() {
     }
   }
 
+// deletes all movies documents in collection and inserts test data
+export async function loadMovies() {
+    console.log('load seed data');
+    console.log(movies.length);
+    try {
+      await movieModel.deleteMany();
+      await movieModel.collection.insertMany(movies);
+      console.info(`${movies.length} Movies were successfully stored.`);
+    } catch (err) {
+      console.error(`failed to Load movie Data: ${err}`);
+    }
+  }
+
+  export async function loadGenres() {
+    console.log('load genres data');
+    console.log(genres.length);
+    try {
+      //await genresModel.deleteMany();
+      await genresModel.collection.insertMany(genres);
+      console.info(`${genres.length} Genres were successfully stored.`);
+    } catch (err) {
+      console.error(`failed to Load genre Data: ${err}`);
+    }
+  }
+
 if (process.env.SEED_DB) {
   loadUsers();
+  loadGenres();//you may not need this line if you skipped the exercises
+  loadMovies();//ADD THIS LINE
 }
