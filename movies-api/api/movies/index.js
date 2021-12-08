34afdb076da60a5d92fb3539abb660d118768d25
Ernestas Trakys <20092082@mail.wit.ie>
2021-12-08 20:30:46 +0000

    topic 11 - ive completed all my labs

--- movies-api/api/movies/index.js
+++ movies-api/api/movies/index.js
@@ -3,6 +3,7 @@ import asyncHandler from 'express-async-handler';
 import express from 'express';
 import { movies, movieReviews, movieDetails } from './moviesData';
 import uniqid from 'uniqid'
+import { getUpcomingMovies } from '../tmdb-api';
 
 const router = express.Router(); 
 
@@ -46,6 +47,11 @@ router.get('/:id/reviews', (req, res) => {
         });
     }
 });
+
+router.get('/tmdb/upcoming', asyncHandler( async(req, res) => {
+    const upcomingMovies = await getUpcomingMovies();
+    res.status(200).json(upcomingMovies);
+  }));
 
 //Post a movie review
 router.post('/:id/reviews', (req, res) => {
